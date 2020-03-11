af4a934fa9d1f025d8feac64751a168cef90db23
sut <math323n@edu.campusvejle.dk>
2020-03-11 10:18:35 +0100

    Created orderdetail class

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -4,23 +4,181 @@ namespace Entities
 {
     public class Order
     {
-        public virtual int OrderID { get; set; }
-        public virtual DateTime OrderDate { get; set; }
-        public virtual DateTime RequiredDate { get; set; }
-        public virtual DateTime ShippedDate { get; set; }
-        public virtual int ShipVia { get; set; }
-        public virtual decimal Money { get; set; }
-        public virtual string ShipName { get; set; }
-        public virtual string ShipAddress { get; set; }
-        public virtual string ShipCity { get; set; }
-        public virtual string ShipRegion { get; set; }
-        public virtual string ShipPostalCode { get; set; }
-        public virtual string ShipCountry { get; set; }
+        protected int orderID;
+        protected DateTime orderDate;
+        protected DateTime requiredDate;
+        protected DateTime shippedDate;
+        protected int shipVia;
+        protected decimal money;
+        protected string shipName;
+        protected string shipAddress;
+        protected string shipCity;
+        protected string shipRegion;
+        protected string shipPostalCode;
+        protected string shipCountry;
 
-        public Order()
+        public Order(int orderID, DateTime orderDate, DateTime requiredDate, DateTime shippedDate,
+            int shipVia, decimal money, string shipName, string shipAddress, string shipCity,
+            string shipRegion, string shipPostalCode, string shipCountry)
         {
-
+            OrderID = orderID;
+            OrderDate = orderDate;
+            RequiredDate = requiredDate;
+            ShippedDate = shippedDate;
+            ShipVia = shipVia;
+            Money = money;
+            ShipName = shipName;
+            ShipAddress = shipAddress;
+            ShipCity = shipCity;
+            ShipRegion = shipRegion;
+            ShipPostalCode = shipPostalCode;
+            ShipCountry = shipCountry;
         }
+
+        public virtual int OrderID
+        {
+            get
+            {
+                return orderID;
+            }
+            set
+            {
+                orderID = value;
+            }
+        }
+
+        public virtual DateTime OrderDate
+        {
+            get
+            {
+                return orderDate;
+            }
+            set
+            {
+                orderDate = value;
+            }
+        }
+
+        public virtual DateTime RequiredDate
+        {
+            get
+            {
+                return requiredDate;
+            }
+            set
+            {
+                requiredDate = value;
+            }
+        }
+
+        public virtual DateTime ShippedDate
+        {
+            get
+            {
+                return shippedDate;
+            }
+            set
+            {
+                shippedDate = value;
+            }
+        }
+
+        public virtual int ShipVia
+        {
+            get
+            {
+                return shipVia;
+            }
+            set
+            {
+                shipVia = value;
+            }
+        }
+
+        public virtual decimal Money
+        {
+            get
+            {
+                return money;
+            }
+            set
+            {
+                money = value;
+            }
+        }
+
+        public virtual string ShipName
+        {
+            get
+            {
+                return shipName;
+            }
+            set
+            {
+                shipName = value;
+            }
+        }
+
+        public virtual string ShipAddress
+        {
+            get
+            {
+                return shipAddress;
+            }
+            set
+            {
+                shipAddress = value;
+            }
+        }
+
+        public virtual string ShipCity
+        {
+            get
+            {
+                return shipCity;
+            }
+            set
+            {
+                shipCity = value;
+            }
+        }
+
+        public virtual string ShipRegion
+        {
+            get
+            {
+                return shipRegion;
+            }
+            set
+            {
+                shipRegion = value;
+            }
+        }
+
+        public virtual string ShipPostalCode
+        {
+            get
+            {
+                return shipPostalCode;
+            }
+            set
+            {
+                shipPostalCode = value;
+            }
+        }
+
+        public virtual string ShipCountry
+        {
+            get
+            {
+                return shipCountry;
+            }
+            set
+            {
+                shipCountry = value;
+            }
+        }
+
 
     }
 }
---
+++ Entities/OrderDetail.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class OrderDetail
+    {
+        protected int orderID;
+        protected decimal unitPrice;
+        protected int quantity;
+        protected float discount;
+
+        public OrderDetail(int orderID, decimal unitPrice, int quantity, float discount)
+        {
+            OrderID = orderID;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+            Discount = discount;
+        }
+
+        public virtual int OrderID
+        {
+            get
+            {
+                return orderID;
+            }
+            set
+            {
+                orderID = value;
+            }
+        }
+
+        public virtual decimal UnitPrice
+        {
+            get
+            {
+                return unitPrice;
+            }
+            set
+            {
+                unitPrice = value;
+            }
+        }
+
+        public virtual int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public virtual float Discount
+        {
+            get
+            {
+                return discount;
+            }
+            set
+            {
+                discount = value;
+            }
+        }
+    }
+}
